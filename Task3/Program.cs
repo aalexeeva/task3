31f8f7e50ebe178c9c6b216cbb6c9308b410a0ad
Алексеева Анастасия Николаевна <analekseeva_1@edu.hse.ru>
2017-04-18 11:24:52 +0500

    init

---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task3
+{ 
+    class Program
+    {
+        static void Main()
+        {
+            var number = 0;
+            var ok = true;
+            double result = 0;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Введите действительное число а");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    if (number <= -1) result = 1 / Math.Pow(number, 2);
+                    else if (number > -1 && number < 2) result = Math.Pow(number, 2);
+                    else result = 4;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка при вводе числа");
+                    ok = false;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Ошибка при вводе числа");
+                    ok = false;
+                }
+            } while (!ok);
+            Console.WriteLine("Значение функции в точке {0} равно {1}", number, result);
+        }
+    }
+}
